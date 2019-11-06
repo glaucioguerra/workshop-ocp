64926f3981eea7d172a3b29083be76f1e209b45d
Glaucio Guerra <glaucioguerra@gmail.com>
2019-11-06 16:34:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Openshift Workshop v2.0</h1> ";
+echo "<h1>Openshift Workshop v3.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
